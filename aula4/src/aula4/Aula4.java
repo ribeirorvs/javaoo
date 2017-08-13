55e630a8d231378454fc614986741b9fcf492f5d
Rodrigo Ribeiro <ribeiro.rvs@hotmail.com>
2017-08-13 00:31:12 -0300

    Included methos setters, getters and contruct

---
+++ aula4/src/aula4/Aula4.java
@@ -0,0 +1,10 @@
+package aula4;
+
+public class Aula4 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", 0.5f, true, "azul");
+        
+    }
+    
+}
---
+++ aula4/src/aula4/Caneta.java
@@ -0,0 +1,49 @@
+package aula4;
+
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+
+    public Caneta(String modelo, float ponta, boolean tampada, String cor) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.tampada = tampada;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
+   
+}
